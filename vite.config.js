8d41e262f96f75346a0daa8b670611455a4bb70f
Suraj <surajrajput733@gmail.com>
2026-05-10 21:54:53 +0530

    fix: Ensure base path is set correctly in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
