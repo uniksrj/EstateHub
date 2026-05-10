75f1fc82e62926fb72f74587d249a17a79661402
Suraj <surajrajput733@gmail.com>
2026-05-10 22:04:02 +0530

    fix: Update base path in Vite configuration for correct asset loading

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
